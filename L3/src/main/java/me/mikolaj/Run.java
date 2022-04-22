12ae1bf0b882711060a53040d79a7d93d39695fc
Dybol <mdyblik01@gmail.com>
2022-04-22 22:04:21 +0200

    Add possibility to enter arguments (n, k)

--- L3/src/main/java/me/mikolaj/Run.java
+++ L3/src/main/java/me/mikolaj/Run.java
@@ -8,20 +8,20 @@ import me.mikolaj.utils.Pair;
 
 public class Run {
 	public static void main(String[] args) {
-
-		int dataLength = 30;
+		int dataLength = args.length == 0 ? 30 : Integer.parseInt(args[0]);
+		int k = args.length == 0 ? 5 : Integer.parseInt(args[1]);
 
 		RandomTabGenerator generator = new RandomTabGenerator();
 		int[] tab = generator.generate(dataLength);
 		int[] tab2 = tab.clone();
 		RandomSelect randomSelect = new RandomSelect();
 
-		Pair<Integer, AlgorithmStatistics> integerAlgorithmStatisticsPair = randomSelect.countPositionalStatistic(dataLength, 5, tab);
+		Pair<Integer, AlgorithmStatistics> integerAlgorithmStatisticsPair = randomSelect.countPositionalStatistic(dataLength, k, tab);
 		System.out.println(integerAlgorithmStatisticsPair.getFirst());
 		System.out.println(integerAlgorithmStatisticsPair.getSecond());
 
 		Select select = new Select();
-		Pair<Integer, AlgorithmStatistics> statsForSelect = select.countPositionalStatistic(dataLength, 5, tab2);
+		Pair<Integer, AlgorithmStatistics> statsForSelect = select.countPositionalStatistic(dataLength, k, tab2);
 		System.out.println(statsForSelect.getFirst());
 		System.out.println(statsForSelect.getSecond());
 	}
