04dd87b3925e6d2d454dd124a27adf99eac19e8f
Dybol <mdyblik01@gmail.com>
2022-05-17 18:09:29 +0200

    Add RB-BST

--- L4/src/main/java/me/mikolaj/Run.java
+++ L4/src/main/java/me/mikolaj/Run.java
@@ -1,11 +1,14 @@
 package me.mikolaj;
 
 import me.mikolaj.algorithms.BST;
+import me.mikolaj.algorithms.RBBst;
+import me.mikolaj.data.RBBstTreeItem;
 import me.mikolaj.generators.RandomTabGenerator;
 
 public class Run {
 	public static void main(String[] args) {
-		ex1();
+//		ex1();
+		ex3();
 	}
 
 	public static void ex1() {
@@ -23,5 +26,21 @@ public class Run {
 		bst.deleteRandomElements(50);
 
 		System.out.println(bst.countHeight());
+	}
+
+	public static void ex3() {
+		RBBst rbBst = new RBBst();
+		rbBst.insertRandomElements(50);
+		rbBst.deleteRandomElements(50);
+
+		rbBst.setRoot((RBBstTreeItem) null);
+
+		System.out.println("-------------------------------------");
+		RandomTabGenerator tabGenerator = new RandomTabGenerator();
+		int[] ascendingArray = tabGenerator.generateAscendingArray(50);
+		rbBst.insertFromArray(ascendingArray);
+		rbBst.deleteRandomElements(50);
+
+		System.out.println(rbBst.countHeight());
 	}
 }
